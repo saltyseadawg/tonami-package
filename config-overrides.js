ddb8a21ea14c66c883db4f4a43a34715bc084f63
Robyn Ching <rjching@uwaterloo.ca>
2022-02-09 16:26:37 -0500

    Documentation

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,7 @@
-/* config-overrides.js */
+/* 
+React App does not allow public or src folder to import outside of themselves
+This, with react-app-rewired package, override this
+*/
 
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 
