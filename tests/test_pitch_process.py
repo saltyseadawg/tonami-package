79df0260d9a024375713d592db1c230de87ff6a3
saltyseadawg <sabrinayu7@gmail.com>
2022-02-17 11:57:45 -0500

    test: add unittests for pitch_process functions - add test cmds to Makefile to run tests easily

--- tests/test_pitch_process.py
+++ tests/test_pitch_process.py
@@ -1,7 +1,18 @@
-from tonami import utils
+import numpy as np
+
+from tonami import pitch_process as pp
 
 def test_moving_average():
+    arr = [1,2,3,4,5,6]
+    result = [1,2,3,4,5,5]
+    assert (pp.moving_average(arr, 3) == result).all()
+
+def test_pad_matrix():
+    arr = np.array([[], [1], [1,2,], [1,2,3,4]], dtype=object)
+    result = np.array([[0,0,0,0],[1,0,0,0], [1,2,0,0], [1,2,3,4]])
+    assert (pp.pad_matrix(arr, 0) == result).all()
+
+def test_get_valid_mask():
     pass
 
-utils.test_print()
 
