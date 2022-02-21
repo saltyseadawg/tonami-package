885495412a39dc0a82b951fb6bf193691da01893
Robyn Ching <rjching@uwaterloo.ca>
2022-02-21 16:10:04 -0500

    Records for 3 seconds, then plays back

--- heroku/audio_btn.py
+++ heroku/audio_btn.py
@@ -7,20 +7,60 @@ from streamlit_bokeh_events import streamlit_bokeh_events
 from bokeh.io import curdoc
 from bokeh.plotting import figure, output_file, show
 
-def audio_btn():  
-    recording = False
+def audio_btn():
     stt_button = Button(label="Record", button_type="primary", id="stt_button")
     stt_button.js_on_event("button_click", CustomJS(args=dict(btn=stt_button), code="""
-        if (btn.label == "Record") {
-            btn.label = "Stop"
-            btn.button_type = "danger"
-            document.dispatchEvent(new CustomEvent("ON_RECORD", {detail: {url: null}}));
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            console.log('getUserMedia supported.');
+            navigator.mediaDevices.getUserMedia (
+                // constraints - only audio needed for this app
+                {
+                    audio: true
+                })
+
+                // Success callback
+                .then(function(stream) {
+                    const mediaRecorder = new MediaRecorder(stream);
+                    mediaRecorder.start();
+
+                    const audioChunks = [];
+                    mediaRecorder.addEventListener("dataavailable", event => {
+                        audioChunks.push(event.data);
+                    });
+
+                    mediaRecorder.addEventListener("stop", () => {
+                        const audioBlob = new Blob(audioChunks);
+                        const audioUrl = URL.createObjectURL(audioBlob);
+                        const audio = new Audio(audioUrl);
+                        audio.play();
+                        document.dispatchEvent(new CustomEvent("ON_RECORD", {detail: {url: audioUrl}}));
+                    });
+
+                    setTimeout(() => {
+                        mediaRecorder.stop();
+                    }, 3000);
+                })
+
+                // Error callback
+                .catch(function(err) {
+                    console.log('The following getUserMedia error occurred: ' + err);
+                }
+            );
+            if (btn.label == "Record") {
+                btn.label = "Stop"
+                btn.button_type = "danger"
+                // document.dispatchEvent(new CustomEvent("ON_RECORD", {detail: {url: null}}));
+            } else {
+                btn.label = "Record"
+                btn.button_type = "primary"
+                // document.dispatchEvent(new CustomEvent("ON_RECORD", {detail: {url: "url"}}));
+            }
+            btn.change.emit()
         } else {
-            btn.label = "Record"
-            btn.button_type = "primary"
-            document.dispatchEvent(new CustomEvent("ON_RECORD", {detail: {url: "url"}}));
+            console.log('getUserMedia not supported on your browser!');
         }
-        btn.change.emit()
+
+        
     """))
 
     result = streamlit_bokeh_events(
@@ -41,4 +81,3 @@ def audio_btn():
                 st.write(url)
 
     st.write(result)
-    st.write(recording)
