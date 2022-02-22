0ca069cfee720b86cf87704f0d75c764aff98543
Robyn Ching <rjching@uwaterloo.ca>
2022-02-21 23:06:30 -0500

    One page, plays sample, replay doesn't work

--- heroku/tonami_interface.py
+++ heroku/tonami_interface.py
@@ -10,84 +10,35 @@ import json
 
 st.set_page_config( "Tonami", "🌊", "centered", "collapsed" )
 
+if 'key' not in st.session_state:
+    st.session_state.key = 0
+
 # Opening JSON file
 with open('heroku/interface_text.json') as json_file:
     data = json.load(json_file)
-    sidebar = data['sidebar']
-    options = sidebar['options']
 
-    st.write(data['title'])
-    
-    # sidebar interface
-    st.sidebar.header(sidebar['header'])
-    option = st.sidebar.selectbox( sidebar['text'], options, 1)
-    
-    key = options.index(option)
+exercises = data["exercises"]
 
-    st.write("Key: ", key, " Name: ", options[key])
+st.write(data['title'])
 
-    if key == 0:
-        st.write(data['instructions'])
-    else:
-        audio_btn()
+if st.session_state.key == 0:
+    st.write(data['instructions'])
+else:
+    exercise = exercises[st.session_state.key - 1]
+    st.write("Test ", str(st.session_state.key), " - ", exercise["character"])
 
-# # Collects user input features into dataframe
-# uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
-# if uploaded_file is not None:
-#     input_df = pd.read_csv(uploaded_file)
-# else:
-#     def user_input_features():
-#         island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
-#         sex = st.sidebar.selectbox('Sex',('male','female'))
-#         bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
-#         bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
-#         flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
-#         body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
-#         data = {'island': island,
-#                 'bill_length_mm': bill_length_mm,
-#                 'bill_depth_mm': bill_depth_mm,
-#                 'flipper_length_mm': flipper_length_mm,
-#                 'body_mass_g': body_mass_g,
-#                 'sex': sex}
-#         features = pd.DataFrame(data, index=[0])
-#         return features
-#     input_df = user_input_features()
+    audio_file = open("data/" + exercise["fileName"] + ".mp3", 'rb')
+    audio_bytes = audio_file.read()
+    st.audio(audio_bytes, format='audio/mp3')
 
-# # Combines user input features with entire penguins dataset
-# # This will be useful for the encoding phase
-# penguins_raw = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-# penguins = penguins_raw.drop(columns=['species'], axis=1)
-# df = pd.concat([input_df,penguins],axis=0)
+    url = audio_btn()
+    if url is not None:
+        st.write(url.replace("blob:",""))
+        # audio_file = open(url, 'rb')
+        # audio_bytes = audio_file.read()
+        st.audio(url.replace("blob:",""))
 
-# # Encoding of ordinal features
-# # https://www.kaggle.com/pratik1120/penguin-dataset-eda-classification-and-clustering
-# encode = ['sex','island']
-# for col in encode:
-#     dummy = pd.get_dummies(df[col], prefix=col)
-#     df = pd.concat([df,dummy], axis=1)
-#     del df[col]
-# df = df[:1] # Selects only the first row (the user input data)
+def increment_key():
+    st.session_state.key += 1
 
-# # Displays the user input features
-# st.subheader('User Input features')
-
-# if uploaded_file is not None:
-#     st.write(df)
-# else:
-#     st.write('Awaiting CSV file to be uploaded. Currently using example input parameters (shown below).')
-#     st.write(df)
-
-# # Reads in saved classification model
-# load_clf = pickle.load(open('penguins_clf.pkl', 'rb'))
-
-# # Apply model to make predictions
-# prediction = load_clf.predict(df)
-# prediction_proba = load_clf.predict_proba(df)
-
-
-# st.subheader('Prediction')
-# penguins_species = np.array(['Adelie','Chinstrap','Gentoo'])
-# st.write(penguins_species[prediction])
-
-# st.subheader('Prediction Probability')
-# st.write(prediction_proba)
+st.button('Next', on_click=increment_key)
